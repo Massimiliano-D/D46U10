8761238ee03eb2040dc30a8e01eb7b7bcfb19391
Massimiliano-D <137884771+Massimiliano-D@users.noreply.github.com>
2023-10-02 16:09:11 +0200

    Nuovo elemento Array
    es. 1.C

--- EsercizioN1.java
+++ EsercizioN1.java
@@ -5,12 +5,30 @@ public class EsercizioN1 {
 
         String risultatoConcatena = concatena ("10", 3 );
         System.out.println("IL TUO RISULTATO=" + risultatoConcatena  );
-        
+
+        String[] arrayIniziale = { "1", "2", "3", "4", "5"};
+        String elementoInserito = "elementoInserito";
+        String[] nuovoArray = inserisciInArray(arrayIniziale, elementoInserito );
+        System.out.println("Nuovo Array:");
+        for (String elemento : nuovoArray){
+            System.out.println(elemento);
+        }
+
     }
     public static int moltiplica(int a, int b) {
         return a * b;
     }
     public static String concatena(String str, int num ) {
         return str + num;
+    }
+    public static String[] inserisciInArray(String[] array, String elemento){
+        String[] nuovoArray = new String[6];
+        nuovoArray[0] = array[0];
+        nuovoArray[1] = array[1];
+        nuovoArray[2] = elemento;
+        nuovoArray[3] = array[3];
+        nuovoArray[4] = array[4];
+
+        return  nuovoArray;
     }
 };

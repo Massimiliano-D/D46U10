840c24cbf419f4c52b7dfb526e92dd73e8f4f91f
Massimiliano-D <137884771+Massimiliano-D@users.noreply.github.com>
2023-10-02 15:35:04 +0200

    es. Concatena
    es 1.B

--- EsercizioN1.java
+++ EsercizioN1.java
@@ -1,9 +1,16 @@
 public class EsercizioN1 {
     public static void main(String[] args) {
-     int risultatoMoltiplica = moltiplica (10, 3);
-        System.out.println("IL TUO RISULTATO=" + risultatoMoltiplica  );
+        int risultatoMoltiplica = moltiplica (10, 3);
+        System.out.println("Moltiplica" + "=" + " " + risultatoMoltiplica  );
+
+        String risultatoConcatena = concatena ("10", 3 );
+        System.out.println("IL TUO RISULTATO=" + risultatoConcatena  );
+        
     }
     public static int moltiplica(int a, int b) {
         return a * b;
+    }
+    public static String concatena(String str, int num ) {
+        return str + num;
     }
 };

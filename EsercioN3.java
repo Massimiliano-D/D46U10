065ba2f4a1edc561af50e3258914ae50bcdefaf6
Massimiliano-D <137884771+Massimiliano-D@users.noreply.github.com>
2023-10-02 16:58:46 +0200

    es Perimetro
    es 3.A

--- EsercioN3.java
+++ EsercioN3.java
@@ -1,2 +1,9 @@
 public class EsercioN3 {
+    public static void main(String[] args) {
+    int Rettangolo= perimetro (10, 3);
+    System.out.println("Perimetro" + "=" + " " + Rettangolo*2);
+}
+    public static int perimetro(int a, int b) {
+        return a + b;
+    }
 }

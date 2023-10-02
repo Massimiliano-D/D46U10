f7c4f01b1137659a2d6b2ff06f7ba1baf6cf20b9
Massimiliano-D <137884771+Massimiliano-D@users.noreply.github.com>
2023-10-02 14:53:55 +0200

    MOLTIPLICA
    es. n-1.A

---
+++ EsercizioN1.java
@@ -0,0 +1,9 @@
+public class EsercizioN1 {
+    public static void main(String[] args) {
+     int risultatoMoltiplica = moltiplica (10, 3);
+        System.out.println("IL TUO RISULTATO=" + risultatoMoltiplica  );
+    }
+    public static int moltiplica(int a, int b) {
+        return a * b;
+    }
+};
